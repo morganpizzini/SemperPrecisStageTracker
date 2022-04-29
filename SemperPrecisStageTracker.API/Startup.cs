ecf8da5e2e0fdcb930b61a871a86cb0de87257fa
Morgan Pizzini <morgan.pizzini@hotmail.com>
2022-04-29 13:30:54 +0200

    public constructure

--- SemperPrecisStageTracker.API/Startup.cs
+++ SemperPrecisStageTracker.API/Startup.cs
@@ -112,7 +112,7 @@ namespace SemperPrecisStageTracker.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env,IConfiguration configuration)
         {
-            if (!env.IsDevelopment())
+            if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
