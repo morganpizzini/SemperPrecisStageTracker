b913e3b2e1916c05fd9e61e434835d0c22a0f269
Morgan Pizzini <morgan.pizzini@hotmail.com>
2022-07-12 13:48:57 +0200

    migration

--- SemperPrecisStageTracker.EF/Migrations/20220712114829_PaymentSpecs.Designer.cs
+++ SemperPrecisStageTracker.EF/Migrations/20220712114829_PaymentSpecs.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SemperPrecisStageTracker.EF.Context;
 
@@ -11,9 +12,10 @@ using SemperPrecisStageTracker.EF.Context;
 namespace SemperPrecisStageTracker.EF.Migrations
 {
     [DbContext(typeof(SemperPrecisStageTrackerContext))]
-    partial class SemperPrecisStageTrackerContextModelSnapshot : ModelSnapshot
+    [Migration("20220712114829_PaymentSpecs")]
+    partial class PaymentSpecs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ SemperPrecisStageTracker.EF/Migrations/20220712114829_PaymentSpecs.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SemperPrecisStageTracker.EF.Migrations
+{
+    public partial class PaymentSpecs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpireDateTime",
+                table: "ShooterTeamPayments");
+
+            migrationBuilder.DropColumn(
+                name: "NotifyExpiration",
+                table: "ShooterTeamPayments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ShooterId",
+                table: "ShooterTeamPayments",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentType",
+                table: "ShooterTeamPayments",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "CompetitionReady",
+                table: "Matches",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TeamId",
+                table: "Matches",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "GroupDay",
+                table: "Groups",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "PaymentTypes",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    TeamId = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    CreationDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TeamReminder",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    TeamId = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    ShooterId = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: true),
+                    Reason = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    ExpireDateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NotifyExpiration = table.Column<bool>(type: "bit", nullable: false),
+                    CreationDateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TeamReminder", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentTypes");
+
+            migrationBuilder.DropTable(
+                name: "TeamReminder");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentType",
+                table: "ShooterTeamPayments");
+
+            migrationBuilder.DropColumn(
+                name: "CompetitionReady",
+                table: "Matches");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "Matches");
+
+            migrationBuilder.DropColumn(
+                name: "GroupDay",
+                table: "Groups");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ShooterId",
+                table: "ShooterTeamPayments",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(255)",
+                oldMaxLength: 255,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpireDateTime",
+                table: "ShooterTeamPayments",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "NotifyExpiration",
+                table: "ShooterTeamPayments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
