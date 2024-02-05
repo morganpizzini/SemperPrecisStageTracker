345188019745904f4f38eafb3823aca23027f7de
Morgan Pizzini <morgan.pizzini@icubed.it>
2024-02-05 17:56:36 +0100

    clone

---
+++ SemperPrecisStageTracker.Shared/Utils/CloneEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+
+namespace SemperPrecisStageTracker.Shared.Utils;
+public static class CloneEntity
+{
+    public static T Clone<T>(this T source)
+    {
+        var serialized = JsonSerializer.Serialize(source);
+        return JsonSerializer.Deserialize<T>(serialized);
+    }
+}
